01414a5d9f2d7367250c972fbc1c5ba2ae85b7ec
“solarc3” <ignacio.solar@usach.cl>
2025-03-22 19:43:36 -0300

    feat: Starting commit
    falta iniciar front

---
+++ backend/src/main/java/tingeso/karting/KartingApplication.java
@@ -0,0 +1,13 @@
+package tingeso.karting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tingeso/karting/KartingApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.karting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KartingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
