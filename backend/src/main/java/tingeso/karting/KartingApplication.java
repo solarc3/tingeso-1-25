54816226f1450a95a1b56a8b635c09b471230ada
“solarc3” <ignacio.solar@usach.cl>
2025-03-22 23:39:18 -0300

    LiveReload y hotchange listo. Faltaba configurar el remote server para que se pudiera usar en local.
    Generar prod dockerfiles con jenkins o empezar a programar

--- backend/src/main/java/tingeso/karting/KartingApplication.java
+++ backend/src/main/java/tingeso/karting/KartingApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class KartingApplication {
 	@RequestMapping("/")
 	public String home() {
-		return "Hello Docker World";
+		return "<html><body><h1>Hoaala World</h1><script src=\"http://localhost:35729/livereload.js\"></script></body></html>";
 	}
 
 	public static void main(String[] args) {
