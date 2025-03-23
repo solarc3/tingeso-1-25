27488dc76eaa65065e256b8f9849cb28d7b26b6c
“solarc3” <ignacio.solar@usach.cl>
2025-03-22 21:40:20 -0300

    primer test de dev build

--- backend/src/main/java/tingeso/karting/KartingApplication.java
+++ backend/src/main/java/tingeso/karting/KartingApplication.java
@@ -2,9 +2,16 @@ package tingeso.karting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KartingApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "Hello Docker World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KartingApplication.class, args);
